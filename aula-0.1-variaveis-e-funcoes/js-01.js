3e3351c0917f6ad479270348af4677ed09251d04
samsesz <samiamunizmuniz@gmail.com>
2025-02-13 19:01:00 -0300

    Variáveis e funções no JS

--- aula-0.1-variaveis-e-funcoes/js-01.js
+++ aula-0.1-variaveis-e-funcoes/js-01.js
@@ -1,18 +1,24 @@
 // comentário em linha
 /* comentário
 em bloco */
+// CRTL + ; = Atalho
 
 // declaração de variáveis em js
 // Cons ou Let ou Var
 // no escopo global const não pode ser alterada
 
-//const precisa de valor 
+const nome = "Diego" //const precisa de valor 
 let Nome  //permite iniciar a variável vazia
+
+// const nome = "Pedro" -> Resulta em um erro
+// Let nome -> Resulta em um erro
+var cor = "Azul"
+var cor = "Amarelo"
 let cidade = "Registro"
-cidade = "Jacupiranga"
+cidade = "Jacupiranga"  // Let permite alterar o valor
 
 
 
-const message = "Hello world"
 
+const message = "Hello world!"
 console.log(message)
