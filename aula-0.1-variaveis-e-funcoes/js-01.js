4ea119f21bcc9d0ea3f9195d69165e8e546fc554
samsesz <samiamunizmuniz@gmail.com>
2025-02-13 18:44:43 -0300

    Váriaveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-01.js
@@ -0,0 +1,18 @@
+// comentário em linha
+/* comentário
+em bloco */
+
+// declaração de variáveis em js
+// Cons ou Let ou Var
+// no escopo global const não pode ser alterada
+
+//const precisa de valor 
+let Nome  //permite iniciar a variável vazia
+let cidade = "Registro"
+cidade = "Jacupiranga"
+
+
+
+const message = "Hello world"
+
+console.log(message)
